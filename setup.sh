768eaeb3d2cd7ff65f4609c31b1b91b156473624
yotamzf <yotam.zeevi.federman@gmail.com>
2024-09-18 16:55:43 +0300

    Added setup.sh so user could install all dependencies and run website in one commans

---
+++ setup.sh
@@ -0,0 +1,28 @@
+# Navigate to backend and install dependencies
+echo "Installing backend dependencies..."
+cd backend/src || exit
+npm install
+
+# Navigate to frontend and install dependencies
+echo "Installing frontend dependencies..."
+cd ../../frontend/src || exit
+npm install
+
+# Navigate back to backend and install Python dependencies
+echo "Installing Python dependencies..."
+cd ../../backend/src || exit
+pip install -r requirements.txt
+
+echo "All dependencies installed successfully."
+
+# Starting backend and frontend
+echo "Starting the backend and frontend..."
+# Start backend
+cd ../../backend/src || exit
+nohup npm start &
+
+# Start frontend
+cd ../../frontend/src || exit
+nohup npm start &
+
+echo "Backend and frontend are now running."
